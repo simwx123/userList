e3383a28c835540d63fea72077d032cfb3c09357
simwx123 <user@Users-MacBook-Pro-2.local>
2021-02-21 16:39:33 +0800

    - Change test format.

--- src/util/common.js
+++ src/util/common.js
@@ -1,17 +1,20 @@
 export function modelKey(prefix, action) {
-  action = Object.assign({ page: -1, size: -1 }, action || {});
-  return `${prefix}${action.page}:${action.size}`;
+	action = Object.assign({
+		page: -1,
+		size: -1
+	}, action || {});
+	return `${prefix}${action.page}:${action.size}`;
 }
 
 export function listmap(list, key = "id") {
-  if (!Array.isArray(list) || !list.length) return {};
-  return list.reduce((acc, item) => {
-    acc[item[key]] = item;
-    return acc;
-  }, {});
+	if (!Array.isArray(list) || !list.length) return {};
+	return list.reduce((acc, item) => {
+		acc[item[key]] = item;
+		return acc;
+	}, {});
 }
 
 export default {
-  modelKey,
-  listmap,
+	modelKey,
+	listmap,
 };
